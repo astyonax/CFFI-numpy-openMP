7b67c1adfc7e19b3ce37646d3c612c2d9123c2ea
nszceta <adam.gradzki@gmail.com>
2015-11-05 15:14:59 -0800

    Work around limitation of CFFI for 2D arrays of variable length

--- test.h
+++ test.h
@@ -1,2 +1,2 @@
 double myadder(double x, double y);
-void add_array(int x, int y, double (*a)[3], double (*b)[3], double (*result)[3]);
+void add_array(int x, int y, double (*a)[NUM_COLS], double (*b)[NUM_COLS], double (*result)[NUM_COLS]);
--- test.py
+++ test.py
@@ -1,3 +1,25 @@
+import numpy
+a = numpy.array(
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ], dtype=numpy.float64
+)
+
+b = numpy.array(
+    [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+    ], dtype=numpy.float64
+)
+
+result = numpy.zeros_like(a)
+
+num_rows = a.shape[0]
+num_cols = a.shape[1]
+
 import sys
 use_openmp = False
 try:
@@ -10,7 +32,8 @@ import cffi
 ffi = cffi.FFI()
 
 with open('test.h') as my_header:
-    ffi.cdef(my_header.read())
+    ffi.cdef(my_header.read().replace('NUM_COLS', str(num_cols)))  # IMPORTANT! cffi limitation
+
 with open('test.c') as my_source:
     if __debug__:
         print('Building the debug build...')
@@ -35,30 +58,12 @@ with open('test.c') as my_source:
 ffi.compile()  # convert and compile - mandatory!
 
 import _test
+
 assert (_test.lib.myadder(10,12)) - (10+12) < 0.01  # A - B < 0.1 if A ~= B
 
-import numpy
-a = numpy.array(
-    [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-    ], dtype=numpy.float64
-)
-
-b = numpy.array(
-    [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9],
-    ], dtype=numpy.float64
-)
-
-result = numpy.zeros_like(a)
-
 _test.lib.add_array(
-    _test.ffi.cast('int', a.shape[0]),
-    _test.ffi.cast('int', a.shape[1]),
+    _test.ffi.cast('int', num_rows),
+    _test.ffi.cast('int', num_cols),
     _test.ffi.cast('double (*)[3]', a.__array_interface__['data'][0]),
     _test.ffi.cast('double (*)[3]', b.__array_interface__['data'][0]),
     _test.ffi.cast('double (*)[3]', result.__array_interface__['data'][0]),
