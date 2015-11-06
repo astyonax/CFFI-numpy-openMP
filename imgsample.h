3b4f4badbbd67e1c7bcad344684ddd984c28ab08
nszceta <adam.gradzki@gmail.com>
2015-11-06 00:51:03 -0800

    3D sampler working 100%

--- imgsample.h
+++ imgsample.h
@@ -1,1 +1,2 @@
 void sample2d(size_t row_count, size_t column_count, size_t window_size, float *input, float *output);
+void sample3d(size_t row_count, size_t column_count, size_t window_size, float *input, float *output);
