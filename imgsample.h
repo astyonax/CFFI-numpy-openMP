a1291fcb26d40566211d11758d5d2b9bd2a21ed2
nszceta <adam.gradzki@gmail.com>
2015-11-05 23:59:43 -0800

    Image sampling proof of concept completed

--- imgsample.h
+++ imgsample.h
@@ -1,1 +1,1 @@
-void sample10(size_t row_count, size_t column_count, size_t window_size, float *input, float *output);
+void sample(size_t row_count, size_t column_count, size_t window_size, float *input, float *output);
--- imgsample.py
+++ imgsample.py
@@ -25,7 +25,7 @@ ffi.compile()  # convert and compile - mandatory!
 
 import _imgsample
 
-window_size = 2
+# window size 2
 
 my_input = numpy.array(
     [
@@ -35,18 +35,20 @@ my_input = numpy.array(
         [13, 14, 15, 16],
     ], dtype=numpy.float32
 )
-my_output = numpy.zeros((9, 2, 2,), dtype=numpy.float32)
+
+window_size = 2
+sample_count = (my_input.shape[0]-1) * (my_input.shape[1]-1)
+my_output = numpy.zeros((sample_count, window_size, window_size), dtype=numpy.float32)
 
 _x = _imgsample.ffi.cast('size_t', my_input.shape[0])
 _y = _imgsample.ffi.cast('size_t', my_input.shape[1])
 _window_size = _imgsample.ffi.cast('size_t', window_size)
-_input = _imgsample.ffi.cast('float *', _imgsample.ffi.from_buffer(my_input))
-_output = _imgsample.ffi.cast('float *', _imgsample.ffi.from_buffer(my_output))
+_my_input = _imgsample.ffi.cast('float *', _imgsample.ffi.from_buffer(my_input))
+_my_output = _imgsample.ffi.cast('float *', _imgsample.ffi.from_buffer(my_output))
 
-_imgsample.lib.sample10(_x, _y, _window_size, _input, _output)
-assert numpy.arrayequal(output[0], [[1,2],[3,4]])
-print('final ->')
-print(my_input)
-print(my_input.shape)
-print(my_output)
-print(my_output.shape)
+_imgsample.lib.sample(_x, _y, _window_size, _my_input, _my_output)
+assert numpy.array_equal(my_output[0], [[1,2],[5,6]])
+assert numpy.array_equal(my_output[sample_count-1], [[11,12],[15,16]])
+
+# window size 3
+
